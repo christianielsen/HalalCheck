693f0ea04ed6fed70bb00ebe2c78e1f766186d14
Chris <chubby1226@gmail.com>
2023-10-16 20:36:38 +0100

    Fixed switch having wrong colour

--- app/src/main/java/com/chris/firebaseauth/settings/SettingsFragment.java
+++ app/src/main/java/com/chris/firebaseauth/settings/SettingsFragment.java
@@ -3,6 +3,7 @@ package com.chris.firebaseauth.settings;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
